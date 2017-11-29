43635d8a4586338daba65eaecf56f2c723034e9a
liuhaohao <liuhaohao@jianbing.com>
2017-11-29 15:26:24 +0800

    Initial Commit

---
+++ RefreshAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RefreshAnimation
+//
+//  Created by 刘浩浩 on 2017/11/29.
+//  Copyright © 2017年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
